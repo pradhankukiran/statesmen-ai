05a40f1cc85e4bcd5c91d2d70aa55dcd0a7d69b8
Kiran Kumar Pradhan <pradhankukiran@gmail.com>
2026-05-09 19:19:08 +0530

    feat(next): allow Members API thumbnails via images.remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "members-api.parliament.uk",
+        pathname: "/api/Members/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
